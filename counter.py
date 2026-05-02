883c0ba3c9c2f241c746c96887f7ba33def72286
Piotr Kubacki <191789264+PiotrKuback1@users.noreply.github.com>
2026-05-02 17:07:00 +0100

    changed layout to reflect yesterdays changes

--- counter.py
+++ counter.py
@@ -10,7 +10,7 @@ class button():
         self.height = height
         self.text = text
 
-    def draw(self,win,outline=None):
+    def drawButton(self,win,outline=None):
         #Call this method to draw the button on the screen
         if outline:
             pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
@@ -18,37 +18,36 @@ class button():
         pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
         
         if self.text != '':
-            Smolfont = pygame.font.SysFont("freesansbold",15,False,False)
+            Smolfont = pygame.font.SysFont("freesansbold",50,False,False)
             text = Smolfont.render(self.text, 1, (0,0,0))
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isOver(self, pos):
         #Pos is the mouse position as tuple of (x,y) coordinates
-        mousePressed = pygame.mouse.get_pressed()
         if pos[0] > self.x and pos[0] < self.x + self.width:
             if pos[1] > self.y and pos[1] < self.y + self.height:
-                if mousePressed[0]:
-                    return True
+                return True
         return False
 
 def drawScreen(screen,counterVal):
     screen.fill("orange")
-    IncButton.draw(screen, (0,0,0))
-    DecButton.draw(screen, (0,0,0))
-    ResetButton.draw(screen, (0,0,0))
+    IncButton.drawButton(screen, (0,0,0))
+    DecButton.drawButton(screen, (0,0,0))
+    ResetButton.drawButton(screen, (0,0,0))
     font = pygame.font.SysFont("freesansbold", 30, True, False)
     text = font.render(str(counterVal), True, (255,255,255))
-    counterRect = text.get_rect(topleft=(100, 30))
+    counterRect = text.get_rect(topleft=(25, 17))
     screen.blit(text, counterRect)
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((400, 212))
+    screen = pygame.display.set_mode((400, 200))
     clock = pygame.time.Clock()
     running = True
+    # pygame.event.set_grab(True)    #if uncommented this will lock your mouse in the window use alt f4 to kill
 
-    #checkinf if file exists
+    #checking if file exists
     if os.path.isfile("./save.txt"):
         with open("save.txt", "r") as saveFile:
             #if exists make the counter value the read number
@@ -64,9 +63,9 @@ def main():
 
     #declare all buttons 
     global ResetButton,IncButton, DecButton
-    IncButton = button((255,255,255),     70,70,200,140,"Increment")
-    DecButton = button((255,255,255),     280,70,110,70,"Decrement")
-    ResetButton = button((255,255,255),   280,140,110,70,"Reset")
+    IncButton = button((255,255,255),     0    ,50  ,200  ,150 ,"+")
+    DecButton = button((255,255,255),     200  ,50  ,200  ,75  ,"-")
+    ResetButton = button((255,255,255),   200  ,125 ,200  ,75  ,"0")
 
     while running:
         drawScreen(screen,counterVal)
@@ -80,8 +79,10 @@ def main():
             else:
                 mousePos = pygame.mouse.get_pos()
                 mousePress = pygame.mouse.get_pressed()
+                keyPress = (event.type == pygame.KEYDOWN)
+
                 pygame.event.wait(10)
-                if mousePress[0]:
+                if mousePress[0] or keyPress:
                     if IncButton.isOver(mousePos):
                         counterVal += 1
 
